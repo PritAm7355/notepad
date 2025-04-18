1f82cfb74060d8af19860ecab40316f3ac3df897
yuti <yutzparmar2004@gmail.com>
2025-04-18 15:15:29 +0530

    Calender added

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,10 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "../components/app-sidebar"
+import { Calendar } from "@/components/ui/calendar"
+import React from "react"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -9,6 +12,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <SidebarTrigger />
         {children}
       </main>
+    <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-md border bg-transparent font-mono"
+  />
     </SidebarProvider>
   )
 }
--- src/pages/NoteContainer/notecontainer.tsx
+++ src/pages/NoteContainer/notecontainer.tsx
@@ -1,23 +1,20 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import Note from '../Note/note';
 import "./notecontainer.css";
-import Sidebar1 from "../Sidebar/sidebar"
 
 export default function NoteContainer(props:any) {
   return (
     <div className="note-container">
-      <h2 className='font-semibold text-2xl'>Notes</h2>
-      <Sidebar1/>
+      <h2 className='font-semibold text-3xl'>Notes</h2>
       <div className="note-container_notes custom-scroll">
       {
         props.notes.map((item: any,index: any)=> <Note 
         key={index}
         note={item}
-       
         />)
       }
     </div>
     </div>
   );
-}
+} 
